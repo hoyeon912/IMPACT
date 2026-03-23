747ad6eee873ef5ce71cc2f84ea23308039a984e
Hoyeon Jang <cliched.0-hoedown@icloud.com>
2026-03-23 12:52:20 +0900

    remove old files

--- impact/__init__.py
+++
@@ -1,3 +0,0 @@
-"""IMPACT: A reinforcement learning environment package."""
-
-__version__ = "0.1.0"
--- impact/envs/__init__.py
+++
@@ -1,5 +0,0 @@
-"""IMPACT environment module."""
-
-from impact.envs.base_env import ImpactEnv
-
-__all__ = ["ImpactEnv"]
